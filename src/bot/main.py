0749a9e6da24be0dbb8311440e59e54d90e9dcb0
mark405 <mark200320031.tun@gmail.com>
2022-12-07 20:42:03 +0200

    merge bot into main

--- src/bot/main.py
+++ src/bot/main.py
@@ -1,6 +1,14 @@
 import creator
+import os
+from dotenv import load_dotenv
+from pathlib import Path
 
+dotenv_path = Path('/Users/markzavgorodniy/PycharmProjects/bot/botyara/.env')
+load_dotenv(dotenv_path=dotenv_path)
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_NAME = os.getenv('BOT_NAME')
 
 if __name__ == '__main__':
-    bot = creator.BotCreator.create('YouTubeParserBot', '5864587562:AAEcIRUlerYT9x5RpA1Xy5vQl7GJvtQU2Ss')
+    bot = creator.BotCreator.create(BOT_NAME, BOT_TOKEN)
     bot.run()
